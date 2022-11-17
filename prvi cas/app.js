40d29a81521b4d319659875baeeb47b083af6914
Remzo00 <gusinacremzo00@gmail.com>
2022-11-17 12:27:57 +0100

    Tipovi podataka, komentari...

--- prvi cas/app.js
+++ prvi cas/app.js
@@ -1,1 +1,24 @@
-console.log("Pozdrav");
+console.log("pozdrav iz konzole");
+// alert("pozdrav");
+// document.write("Pozdrav");
+//Komentar
+
+console.log(5 + 5);
+
+//Tipovi podataka
+//PROSTI TIPOVI PODATAKA
+//1. String -> predstavlja neki tekst
+//2. Number -> predstavlja brojevnu vrednost odnosno omogucava da obavimo matematicke operacije
+//3. Boolean -> predstavlja logicku vrednost
+//4. Undefined -> javlja da vrednost nije definisana
+//5. Null -> javlja da vrednost ne postoji
+//6. Symbol -> zastita podataka
+
+var tekst = "Pozdrav";
+var nesto = 5;
+console.log(tekst);
+
+var x = 5;
+var y = 5;
+var z = x + y;
+console.log("Vas zbir je " + z);
