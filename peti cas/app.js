8c4272f09f50dcb2ea3ca5012709c58ff09a65b8
Remzo00 <gusinacremzo00@gmail.com>
2022-12-01 12:17:52 +0100

    Petlje

---
+++ peti cas/app.js
@@ -0,0 +1,47 @@
+//Da bismo proverili nejednakost koristimo:
+// !=
+//trazi razliku
+// !==
+
+// != -> proverava samo vrednost
+// !== -> proverava i tip i vrednost
+
+// for(inicijalnaVrednost; uslov; brojac)
+
+//Pogresno
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+console.log("Hello world");
+
+//Uvecava
+for (let i = 1; i <= 10; i++) {
+  console.log("hello world", i);
+}
+//Umanjuje
+for (let i = 10; i >= 0; i--) {
+  console.log("hello world", i);
+}
+
+// while (uslov) {
+
+// }
+
+let text = "";
+let i = 0;
+
+while (i < 10) {
+  text += "The number is " + i;
+  i++;
+}
+
+//printati brojeve koji su deljivi sa 2. Petlja pocinje od 1 zavrsava se do 10
+
+for (let i = 1; i < 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
