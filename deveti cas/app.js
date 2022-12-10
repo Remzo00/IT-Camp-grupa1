f5f1f380d3a590bc10c3af8e426df06167606b70
Remzo00 <gusinacremzo00@gmail.com>
2022-12-10 12:39:29 +0100

    Vezba

---
+++ deveti cas/app.js
@@ -0,0 +1,14 @@
+//1. Napisati funkciju koja vraca kvadrat broja 2,9,225
+//2. Napisati funkciju za pronalazenje povrsine datog pravougaonika
+
+const pronadjiKvadrat = (broj) => {
+  return broj * broj;
+};
+console.log(pronadjiKvadrat(2));
+console.log(pronadjiKvadrat(9));
+console.log(pronadjiKvadrat(15));
+
+const povrsinaPravougaonika = (a, b) => {
+  return a * b;
+};
+console.log(povrsinaPravougaonika(10, 20));
