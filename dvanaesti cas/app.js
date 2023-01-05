934698d3c55ad08b387e1adfa110940efb0b101b
Remzo00 <gusinacremzo00@gmail.com>
2023-01-05 12:36:19 +0100

    DOM vezbe

--- dvanaesti cas/app.js
+++ dvanaesti cas/app.js
@@ -1,3 +1,16 @@
-let paragraf = (document.getElementById("demo").style.color = "blue");
+//Dohvatanje elementa po id-u
+let paragraf = (document.getElementById("demo").style.color = "red");
 
-// Napraviti dugme koje kad se klikne promeni boju nekog teksta u plavu
+//Dohvatanje elementa po klasi
+const listItems = document.getElementsByClassName("list-group-fruit");
+console.log(listItems);
+function klik() {
+  for (var i = 0; i < listItems.length; i++) {
+    listItems[i].style.color = "red";
+  }
+}
+
+//Dohvatanje elementa po tag-u
+const listItemsTag = document.getElementsByTagName("li");
+
+let p = document.querySelector("demo");
